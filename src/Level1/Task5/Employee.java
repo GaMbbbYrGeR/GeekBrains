98eb2850517808d1f597c4b50812413f626880e1
GaMbbbYrGeR <arshavin.kirill@mail.ru>
2024-07-10 19:07:10 +0300

    5.2

--- src/Level1/Task5/Employee.java
+++ src/Level1/Task5/Employee.java
@@ -1,4 +1,44 @@
 package Level1.Task5;
 
 public class Employee {
+    private String fullName;
+    private String post;
+    private String email;
+    private String phoneNumber;
+    private String salary;
+    private int age;
+
+    public Employee() {
+
+    }
+
+    public Employee(String fullName, String post, String email, String phoneNumber, String salary, int age) {
+        this.fullName = fullName;
+        this.post = post;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void getInfo() {
+        System.out.println("ФИО: " + this.fullName);
+        System.out.println("Должность: " + this.post);
+        System.out.println("Email:" + this.email);
+        System.out.println("Номер телефона: " + this.phoneNumber);
+        System.out.println("Зарплата: " +  this.salary);
+        System.out.println("Возраст: " +   this.age);
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
--- src/Level1/Task5/Main.java
+++ src/Level1/Task5/Main.java
@@ -1,4 +1,29 @@
 package Level1.Task5;
 
 public class Main {
+    public static void main(String[] args) {
+        Employee[] employees = {
+                new Employee("Иванов Иван Иванович", "Стажер-программист", "1@mail.ru",
+                        "89996663322", "20000", 20),
+
+                new Employee("Сергеев Сергей Сергеевич", "Стажер-программист", "12@mail.ru",
+                        "89996664455", "20000", 22),
+
+                new Employee("Кузвецов Кирилл Вадимович", "Программист", "123@mail.ru",
+                        "89918863322", "45000", 21),
+
+                new Employee("Богданов Марк Александрович", "Ведущий программист", "1234@mail.ru",
+                        "89057763322", "180000", 41),
+
+                new Employee("Евдокимов Петр Евгеньевич", "Senior-программист", "123456@mail.ru",
+                        "89991111111", "250000", 45),
+        };
+
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getAge() > 40) {
+                employees[i].getInfo();
+                System.out.println("-----------------------------------------");;
+            }
+        }
+    }
 }
